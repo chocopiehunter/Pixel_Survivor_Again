c3a2b48943394eba68fd98b28d937e45675193b3
jh <wogh4616@gmail.com>
2026-05-22 15:49:30 +0900

    [Init] 초기화

---
+++ Assets/Scripts/DN_2D/DaniTech_GameMonster.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DaniTech_GameMonster : DaniTech_MonsterBase
+{
+    [Header("몬스터 프리팹에서 미리 세팅할 데이터")]
+    public float SkillCoolTime;
+    public GameObject Prefab_ThisMonsterSkillObjct;
+    [SerializeField] private SpriteRenderer SpriteRenderer_Monster;
+
+
+    [Header("데이터를 확인할 수 있도록 임시로 열어줌")]
+    public int _instanceId; // 게임 오브젝트 매니저에서 찾기용(동사무소용) - 게임에서 태어날때 부여된 나의 주민등록번호
+    public string _dataId; // 데이터 드리븐용 - 나의 부가정보를 나중에 찾을 수 있는 Id 
+
+    [Header("받아왔는데 전투에서 필요한 데이터")]
+    private DNMonsterData _thisMonsterData;
+    public int _baseHp;
+    public int _baseAtk;
+    public bool _isAlive = true;
+    private bool _lookRight = true;
+
+    private Vector3 _moveDirection;
+
+    private void OnDisable()
+    {
+        _isAlive = false;
+    }
+
+    // 태어난 시점에서 어떤 정보를 저장해주자
+    public void InitMonster(int instanceId, string dataId)
+    {
+        _instanceId = instanceId;
+        _dataId = dataId;
+
+        // 초기화 한다음에 그 객체가 가지고 있는 데이터를 이렇게 찾아와서 필요한 세팅을 해줍니다.
+        var monsterData = DaniTechGameDataManager.Instance.GetDNMonsterData(dataId);
+        if(monsterData != null)
+        {
+            // 이 몬스터가 생성된 시점에서 자신의 엑셀 -> JSON을 거친 데이터를 캐싱(보관)해둔다
+            _thisMonsterData = monsterData;
+            _baseHp = _thisMonsterData.BaseHp;
+            _baseAtk = _thisMonsterData.BaseAtk;
+        }
+
+        StartCoroutine(CheckAndUseSkill());
+    }
+
+    private int GetFinalNormalAtkDamage(int baseAtk, float normalAtkMultiple)
+    {
+        return GetFinalSkillDamage(baseAtk, normalAtkMultiple);
+    }
+
+    private int GetFinalSkillDamage(int baseAtk, float skillMultiple)
+    {
+        return (int)(baseAtk * skillMultiple);
+    }
+
+    // 코루틴이 등장한다는건 => 유니테스크로 호환이 가능하다
+    // 일정 시간마다 스킬을 사용할 예정
+    // 스타트 코루틴은 이 몬스터가 생성된 시점에서 돌아도 됨!
+    IEnumerator CheckAndUseSkill()
+    {
+        while (_isAlive)
+        {
+            yield return new WaitForSeconds(SkillCoolTime);
+
+            if (_isAlive == false)
+            {
+                break;
+            }
+
+            ChangeMonsterDirection();
+            UseSkill();
+        }
+    }
+
+    void ChangeMonsterDirection()
+    {
+        _lookRight = !_lookRight;
+        _moveDirection = new Vector3(_lookRight ? 1 : -1, 0, 0);
+        SetMeshDirectionByMoveDirection((int)_moveDirection.x);
+    }
+
+
+    void SetMeshDirectionByMoveDirection(int x)
+    {
+        SpriteRenderer_Monster.flipX = (x < 0);
+    }
+
+    private void UseSkill()
+    {
+        // UI에서도 동적생성했듯, 지금 스킬 투사체 오브젝트도 소환!(실체화-동적생성)
+        var gObj = Instantiate(Prefab_ThisMonsterSkillObjct, DaniTechGameObjectManager.Inst.transform);
+        if (gObj == null) return;
+
+        var skillProjectileComponent = gObj.GetComponent<DaniTech_SkillProjectile>();
+        if (skillProjectileComponent == null) return;
+
+        // TODO : 추후 함수로 빠져야함
+        float skillMultiple = _thisMonsterData.SkillAtkMultipleList.Count > 0 ? _thisMonsterData.SkillAtkMultipleList[0] : 0;
+        int finalSkillDamage = GetFinalSkillDamage(_baseAtk, skillMultiple);
+        var tag = this.gameObject.tag;
+        skillProjectileComponent.InitSkillObject(_instanceId, _lookRight, this.transform.position, finalSkillDamage, tag);
+    }
+
+
+}
---
+++ Assets/Scripts/DN_2D/DaniTech_SkillProjectile.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DaniTech_SkillProjectile : DaniTech_SkillBase
+{
+    [SerializeField] private SpriteRenderer SpriteRenderer_Effect;
+    [SerializeField] private float ProjectileSpeed = 5.0f;
+
+    private int _damage;
+    private int _ownerInstanceId; // 나를 소환한 주인의 Id
+
+    // 탑뷰, 아이소메트릭이면 y연산도 추가 될 예정
+    private Vector3 _moveDirection = new Vector3(1, 0, 0); // 사이드뷰 기준으로는 x가 -1,1 좌우 구분
+
+    public void InitSkillObject(int ownerInstanceId, bool isDirRight, Vector3 playerPos, int damage, string parentTag)
+    {
+        this.transform.position = playerPos;
+
+        // 사이드뷰 기준 x값만 좌 우 1 또는 -1로 지정됨
+        _moveDirection = isDirRight ? new Vector3(1, 0, 0) : new Vector3(-1,0,0);
+        SpriteRenderer_Effect.flipX = !isDirRight;
+        SpriteRenderer_Effect.flipY = !isDirRight;
+
+        _damage = damage;
+        _ownerInstanceId = ownerInstanceId;
+
+        // 소환자의 Tag정보를 기입한다
+        this.gameObject.tag = parentTag;
+    }
+
+    private void Update()
+    {
+        transform.position += _moveDirection * ProjectileSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        CheckCollision(collision);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        CheckCollision(collision.collider);
+    }
+
+    private void CheckCollision(Collider2D collision)
+    {
+        // Owner가 0번이면 무조건 플레이어다
+        bool isOwnerPlayer = (_ownerInstanceId == 0);
+
+        // 투사체가 충돌한 오브젝트의 Tag가 플레이어라면?
+        if (collision.CompareTag("Player") == (isOwnerPlayer == false)) 
+        {
+            // 플레이어라면 직접 플레이어에게 투사체가 데미지를 부여해봅시다
+             var player = DaniTechGameObjectManager.Inst.GetLocalPlayer();
+            player.TakeDamage(_damage);
+
+            // 스킬은 오브젝트 매니저를 통해서 만들어지지는 않았으므로 직접 스스로 제거해봅시다
+            // 몬스터 -> 오브젝트 매니저를 통해서 제거 (UI매니저와 동일한 프로세스)
+            // 스킬은 직접 스스로 제거
+            Destroy(this.gameObject);
+        }
+    }
+}
