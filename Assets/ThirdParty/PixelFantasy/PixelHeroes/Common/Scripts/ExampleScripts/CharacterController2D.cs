3d93df964dce7aa71338a1c9aa4cc41321adf589
jh <wogh4616@gmail.com>
2026-06-01 15:50:08 +0900

    [비상] Local Changes(19382) 해결하기2 Assets 전부 커밋(ThirdParty와 Tiles가 각각 9000개씩 문제였음)

--- Assets/ThirdParty/PixelFantasy/PixelHeroes/Common/Scripts/ExampleScripts/CharacterController2D.cs
+++ Assets/ThirdParty/PixelFantasy/PixelHeroes/Common/Scripts/ExampleScripts/CharacterController2D.cs
@@ -9,20 +9,14 @@ namespace Assets.PixelFantasy.PixelHeroes.Common.Scripts.ExampleScripts
     [RequireComponent(typeof(CharacterAnimation))]
     public class CharacterController2D : MonoBehaviour
     {
-        public Vector2 Input;
-        public bool IsGrounded;
+        public Vector2 Input; // 외부 입력 스크립트 등에서 넣어주는 키보드 입력값 (X, Y)
 
-        public float Acceleration;
-        public float MaxSpeed;
-        public float JumpForce;
-        public float Gravity;
+        public float Acceleration; // 가속도
+        public float MaxSpeed;     // 최대 이동 속도
 
         private Collider2D _collider;
         private Rigidbody2D _rigidbody;
         private CharacterAnimation _animation;
-        
-        private bool _jump;
-        private bool _crouch;
 
         public void Awake()
         {
@@ -35,108 +29,67 @@ namespace Assets.PixelFantasy.PixelHeroes.Common.Scripts.ExampleScripts
         {
             var state = _animation.GetState();
 
+            // 사망, 방어, 등반 상태일 때는 이동하지 못하도록 차단
             if (state == CharacterState.Die || state == CharacterState.Block || state == CharacterState.Climb) return;
 
             var velocity = _rigidbody.linearVelocity;
             var maxSpeed = MaxSpeed;
             float accel = Acceleration;
-            float decel = Acceleration * 4f;
+            float decel = Acceleration * 4f; // 정지할 때의 감속도
 
-            if (!IsGrounded)
-            {
-                accel *= 0.8f;
-                decel *= 0.5f;
-            }
-            else if (_crouch)
-            {
-                accel /= 2;
-                maxSpeed /= 4;
-            }
-
+            // --- [X축 이동 처리] ---
             if (Input.x == 0)
             {
+                // 입력이 없으면 부드럽게 멈춤
                 velocity.x = Mathf.MoveTowards(velocity.x, 0, decel * Time.fixedDeltaTime);
             }
             else
             {
+                // 방향을 전환하는 중인지 체크하여 가속도 조절
                 bool isTurning = (Input.x > 0 && velocity.x < 0) || (Input.x < 0 && velocity.x > 0);
                 float currentStrength = isTurning ? (accel + decel) : accel;
 
                 velocity.x = Mathf.MoveTowards(velocity.x, Input.x * maxSpeed, currentStrength * Time.fixedDeltaTime);
-                Turn(velocity.x);
+                Turn(velocity.x); // 이동 방향에 맞춰 스프라이트 좌우 반전
             }
 
-            if (IsGrounded)
+            // --- [Y축 이동 처리 (★탑다운 뷰 추가)] ---
+            if (Input.y == 0)
             {
-                _crouch = Input.y < 0;
-
-                if (!_jump)
-                {
-                    if (Input.x == 0)
-                    {
-                        if (_crouch) _animation.Crouch();
-                        else if (state != CharacterState.Idle) _animation.Ready();
-                    }
-                    else
-                    {
-                        if (_crouch) _animation.Crawl();
-                        else _animation.Run();
-                    }
-                }
-
-                if (Input.y > 0 && !_jump)
-                {
-                    _jump = true;
-                    _rigidbody.AddForce(Vector2.up * JumpForce);
-                    _animation.Jump();
-                }
+                // 입력이 없으면 부드럽게 멈춤
+                velocity.y = Mathf.MoveTowards(velocity.y, 0, decel * Time.fixedDeltaTime);
             }
             else
             {
-                velocity.y -= Gravity * Time.fixedDeltaTime;
+                // 방향을 전환하는 중인지 체크하여 가속도 조절
+                bool isTurning = (Input.y > 0 && velocity.y < 0) || (Input.y < 0 && velocity.y > 0);
+                float currentStrength = isTurning ? (accel + decel) : accel;
 
-                if (velocity.y < 0)
-                {
-                    _jump = true;
-                    _animation.Fall();
-                }
+                velocity.y = Mathf.MoveTowards(velocity.y, Input.y * maxSpeed, currentStrength * Time.fixedDeltaTime);
             }
 
+            // --- [탑다운 뷰 애니메이션 제어] ---
+            if (Input.x == 0 && Input.y == 0)
+            {
+                // 상하좌우 아무 입력도 없으면 대기(Idle) 상태로 변경
+                if (state != CharacterState.Idle) _animation.Ready();
+            }
+            else
+            {
+                // 움직임이 있다면 달리기(Run) 애니메이션 재생
+                _animation.Run();
+            }
+
+            // 최종 계산된 속도를 리지드바디에 적용
             _rigidbody.linearVelocity = velocity;
         }
 
+        // 스프라이트 좌우 반전 함수
         private void Turn(float direction)
         {
             var scale = transform.localScale;
-
             scale.x = Mathf.Sign(direction) * Mathf.Abs(scale.x);
-
             transform.localScale = scale;
-        }
-
-        private Collider2D _ground;
-
-        public void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (collision.contacts.All(i => i.point.y <= _collider.bounds.min.y))
-            {
-                IsGrounded = true;
-                _ground = collision.collider;
-
-                if (_jump)
-                {
-                    _jump = false;
-                    _animation.Land(Input.y < 0 ? CharacterState.Crouch : CharacterState.Land);
-                }
-            }
-        }
-
-        public void OnCollisionExit2D(Collision2D collision)
-        {
-            if (IsGrounded && collision.collider == _ground)
-            {
-                IsGrounded = false;
-            }
         }
     }
 }
